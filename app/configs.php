bc0d0be1211b30cd9552c81f6a52ce3c2a3f11c2
anonymframework <vahit.serif119@gmail.com>
2016-07-27 21:05:07 +0300

    made some updates

--- app/configs.php
+++ app/configs.php
@@ -4,4 +4,10 @@
 return array(
     'default_controller' => 'Home',
 
+    'database' => [
+        'host' => 'localhost',
+        'dbname' => 'test',
+        'username' => 'root',
+        'password' => 'sanane123'
+    ]
 );
